13f3ed2ad7ebf0a0c00491cd31dfe22b9577752f
Miguel Limgenco Calleja <miguelcalleja911@gmail.com>
2018-08-08 10:55:15 -0700

    just added a for loop in the back function so that I could iterate through the rest of the candidates

--- 39.java
+++ 39.java
@@ -1,27 +1,28 @@
 class Solution{
     public List<List<Integer>> combinationSum(int[] candidates, int target){
         List<List<Integer>>  res = new LinkedList<>();
-        for(int i=candidates.length(); i>0; i--){
+        for(int i=candidates.length; i>0; i--){
             List<Integer> temp = new LinkedList<>();
-            back(candidates, target, difference, i, res, temp)
+            back(candidates, target, i, res, temp);
         }
+        return res;
     }
-    public void back(int[] candidates, int target, int difference, int tail, List<List<Integer>> res, List<Integer> temp){
+    public void back(int[] candidates, int target, int tail, List<List<Integer>> res, List<Integer> temp){
         int difference = target - candidates[tail];
         if(difference == 0){
             temp.add(candidates[tail]);
             res.add(temp);
+            temp.remove(temp.size()-1);
             return;
         }
         else if(difference > 0){
             temp.add(candidates[tail]);
             back(candidates, difference, tail, res, temp);
         }
+        for(int j=tail; j>0; j--){
+            back(cadidates, difference, j, res, temp);
+        }
         else if(difference < 0){
-            if(tail>0) {
-                tail--;
-                back(candidates, difference, tail, res, temp);
-            }
             return;
         }
     }
