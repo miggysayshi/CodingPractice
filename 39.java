59955b48d0deaf176d7a3974f696da3f37f8f518
Miguel Limgenco Calleja <miguelcalleja911@gmail.com>
2018-08-07 21:50:03 -0700

    new changes cleaner

--- 39.java
+++ 39.java
@@ -1,10 +1,12 @@
 class Solution{
     public List<List<Integer>> combinationSum(int[] candidates, int target){
+        List<List<Integer>>  res = new LinkedList<>();
         for(int i=candidates.length(); i>0; i--){
-
+            List<Integer> temp = new LinkedList<>();
+            back(candidates, target, difference, i, res, temp)
         }
     }
-    public void rec(int[] candidates, int target, int sum, int tail, List<List<Integer>> res, List<Integer> temp){
+    public void back(int[] candidates, int target, int difference, int tail, List<List<Integer>> res, List<Integer> temp){
         int difference = target - candidates[tail];
         if(difference == 0){
             temp.add(candidates[tail]);
@@ -13,18 +15,14 @@ class Solution{
         }
         else if(difference > 0){
             temp.add(candidates[tail]);
-            rec(candidates, difference, tail, res, temp);
+            back(candidates, difference, tail, res, temp);
         }
         else if(difference < 0){
             if(tail>0) {
                 tail--;
-                rec(candidates, difference, tail, res, temp);
+                back(candidates, difference, tail, res, temp);
             }
             return;
-        }
-
-        for(int pointer=tail; pointer<candidates.length; pointer++){
-
         }
     }
 }
